cae03fb5df1eb63612ab21d28d1b516ec3fabd3b
niharpatil123 <78696646+niharpatil123@users.noreply.github.com>
2022-02-18 12:54:23 +0530

    Update STester2.py

--- STester2.py
+++ STester2.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta, date
 import calendar
 from AccessInitiator import AccessInitiator
 from CalculateGreeks import CalculateGreeks
+import random
+from TelegramMessenger import TelegramMessenger
 import sched, schedule
 import logging
 from selenium import webdriver
@@ -87,7 +89,68 @@ def getActivePositions():
     #print(positionList['hedgeOrder']['callSymbol'])
     return positionList
 
+def cancelSLOrders():
+    print("=======================Cancel SL Orders=======================")
+    orderList = conn.orders()
+    for i in orderList:
+        print(i["order_id"],"::::",i["status"])
+        #TRIGGER PENDING
+        if(i["status"] == "REJECTED" ):
+            cancelOrder(conn, i["order_id"], i["parent_order_id"])
 
+def cancelOrder(conn,orderId,parentOrderId):
+    order = conn.cancel_order(order_id=orderId,variety=conn.VARIETY_REGULAR,parent_order_id=parentOrderId)
+    print("SL Order Successfully cancelled::::")
+
+def getPNL():
+    margin = conn.margins()
+    realized = conn.margins()['equity']['utilised']['m2m_realised']
+    print(realized)
+    unrealized = conn.margins()['equity']['utilised']['m2m_unrealised']
+    print(unrealized)
+    total = realized + unrealized
+    return total
+
+def getPNL2():
+    positionList = conn.positions()
+    pnl = 0
+    for i in positionList['day']:
+        pnl = pnl + i['pnl']
+        print(pnl)
+    print(pnl)
+    return pnl
+
+def getPNL3():
+    positionList = conn.positions()
+    pnl = 0
+    for i in positionList['day']:
+        pnl = pnl + i['m2m']
+        print(pnl)
+    print(pnl)
+    return pnl
+
+def checkPNL():
+    minProfit = trailSLConfig["minProfit"]
+    maxProfit = trailSLConfig["maxProfit"]
+    trailingSL = trailSLConfig["trailingSL"]
+    totalPNL = random.randrange(-19, 15)
+    #totalPNL=getPNL2()
+    print("Total PNL:",totalPNL," maxProfit: ",maxProfit,"  Trailing SL: ",trailingSL)
+    if(totalPNL > maxProfit):
+        maxProfit = totalPNL
+    #tm.telegram_sendmsg("Net Profit::: \\" + str(int(totalPNL)) + "Max Profit::: \\" + str(int(maxProfit)), "0")
+    if (maxProfit > 10):
+        trailingSL = 0
+
+    trailSLConfig["maxProfit"] = maxProfit
+    trailSLConfig["trailingSL"] = trailingSL
+
+    if(totalPNL < trailingSL):
+        #exitAllPositions()
+        print("Exit All Positions")
+        quit()
+
+"""
 hedgeOrder = {"callSymbol":0,"callStrike":0,"callPrice":0,"putSymbol":0,"putStrike":0,"putPrice":0}
 straddleOrder = {"callSymbol":0,"callStrike":0,"callPrice":0,"putSymbol":0,"putStrike":0,"putPrice":0}
 positionDict = {'hedgeOrder': {'callSymbol': 0, 'callStrike': 0, 'callPrice': 0, 'putSymbol': 0, 'putStrike': 0, 'putPrice': 0}, 'straddleOrder': {'callSymbol': 0, 'callStrike': 0, 'callPrice': 0, 'putSymbol': 0, 'putStrike': 0, 'putPrice': 0}}
@@ -112,3 +175,53 @@ setActivePositions(positionDict)
 
 #9.35onwards read from file
 getActivePositions()
+"""
+
+accessToken = "164u6MGyvrCwAA3z2j1h8kNjureyjBqt"
+print(accessToken)
+api_key = "xyipj2fx4akxggck"
+print(api_key)
+conn = KiteConnect(api_key=api_key)
+conn.set_access_token(accessToken)
+print("Start Time: ",datetime.now().hour,datetime.now().minute,datetime.now().second)
+calG = CalculateGreeks(conn)
+positionDict = {'hedgeOrder': {'callSymbol': 0, 'callStrike': 0, 'callPrice': 0, 'putSymbol': 0, 'putStrike': 0, 'putPrice': 0}, 'straddleOrder': {'callSymbol': 0, 'callStrike': 0, 'callPrice': 0, 'putSymbol': 0, 'putStrike': 0, 'putPrice': 0}}
+positionDict["straddleOrder"]["callSymbol"]  = "NFO:BANKNIFTY2220338800CE"
+positionDict["straddleOrder"]["putSymbol"] = "NFO:BANKNIFTY2220338800PE"
+positionList = []
+positionList.append(positionDict["straddleOrder"]["callSymbol"])
+positionList.append(positionDict["straddleOrder"]["putSymbol"])
+#print(positionList)
+#currPrices = conn.ltp(positionList)
+#orderList = conn.orders()
+#print(orderList)
+#for i in orderList:
+   #print(i['order_id'],i['tradingsymbol'],i['transaction_type'],i['guid'],i['average_price'])
+#orderHist = conn.order_history()
+#print(currPrices)
+#print(currPrices[positionDict["straddleOrder"]["callSymbol"]]["last_price"])
+#print(currPrices[positionDict["straddleOrder"]["putSymbol"]]["last_price"])
+    #print(conn.margins()['equity']['utilised']['m2m_realised'])
+    #print(conn.margins())
+print(conn.positions())
+#number = 108.925
+#a = round(0.05 * round(number/0.05),2)
+#print(a)
+#b = a*(1+80/100)
+#print(b)
+#total = getPNL2()
+#print(total)
+maxProfit = 0
+trailingSL = -20
+trailSLConfig = {"minProfit":10,"trailingSL":-20,"maxProfit":0}
+print(trailSLConfig)
+#schedule.every(4).seconds.do(getPNL3)
+cancelSLOrders()
+#while True:
+    #while (datetime.now() > startTime and datetime.now() < endTime):
+    #schedule.run_pending()
+    #time.sleep(1)
+
+
+#tm = TelegramMessenger()
+#tm.telegram_sendmsg(positionDict["straddleOrder"]["callSymbol"]+"::"+str(int(currPrices[positionDict["straddleOrder"]["callSymbol"]]["last_price"]))+"::"+positionDict["straddleOrder"]["putSymbol"]+"::"+str(int(currPrices[positionDict["straddleOrder"]["putSymbol"]]["last_price"])),"0")
