a3a471ad385dbffd74414f64dd52c6378002f37a
niharpatil123 <78696646+niharpatil123@users.noreply.github.com>
2022-02-22 15:39:42 +0530

    Update pnl_test.py

--- pnl_test.py
+++ pnl_test.py
@@ -1,27 +1,22 @@
-def getPnl():
-    positionList = KiteConn.positions()
-    # print(positionList)
-    
-    # pprint(positionList)
-    # file = open("orders/3legActivePositions.json","r")
-    # positionList = json.load(file)
-    totalPnl = 0
-    postionPNL = 0
-    hedgePNL = 0
-    realizedPNL = 0
-    for i in positionList['net']:
-        # print(i['tradingsymbol'], ":::", i['quantity'],":::",i['last_price'],":::",i['sell_price'],":::",i['buy_price'])
-        if (i['quantity'] < 0):
-            postionPNL = postionPNL +  (i['sell_value'] - i['buy_value']) + (i['quantity'] * i['last_price'] * i['multiplier']) 
-            # print(sellpnl)
-        elif (i['quantity'] > 0):
-            hedgePNL = hedgePNL + (i['sell_value'] - i['buy_value']) + (i['quantity'] * i['last_price'] * i['multiplier']) 
-            # print(buypnl)
-        elif (i['quantity'] == 0):
-            # realizedPNL = realizedPNL + (i['sell_price'] - i['buy_price']) * i['sell_quantity']
-            realizedPNL = realizedPNL + (i['sell_value'] - i['buy_value']) + (i['quantity'] * i['last_price'] * i['multiplier']) 
-            # print (realizedPNL)
-            # print(round(closepnl))
-    totalPnl = postionPNL + hedgePNL + realizedPNL
-    print(round(totalPnl))
-    # return totalPnl
+def checkPNL(self):
+        positionList = self.KiteConn.positions()
+        totalPnl = 0
+        postionPNL = 0
+        hedgePNL = 0
+        realizedPNL = 0
+        for i in positionList['net']:
+            tradeSymbol = 'NFO:' + i['tradingsymbol']
+            ohlc = self.KiteConn.ohlc(tradeSymbol)
+            lastPrice = ohlc[tradeSymbol]['last_price']
+            print(lastPrice)
+            if (i['quantity'] < 0):
+                postionPNL = postionPNL +  (i['sell_value'] - i['buy_value']) + (i['quantity'] * lastPrice * i['multiplier']) 
+            elif (i['quantity'] > 0):
+                hedgePNL = hedgePNL + (i['sell_value'] - i['buy_value']) + (i['quantity'] * lastPrice * i['multiplier']) 
+            elif (i['quantity'] == 0):
+                realizedPNL = realizedPNL + (i['sell_value'] - i['buy_value']) + (i['quantity'] * lastPrice * i['multiplier']) 
+                # print (realizedPNL)
+        totalPnl = round(postionPNL + hedgePNL + realizedPNL)
+        print(totalPnl)
+        self.sendTelegram("Net Profit: \\" + str(int(totalPnl)))
+        # return totalPnl
